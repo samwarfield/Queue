4f10f5e99c39276f5d618ea88d1806d8326b9418
Adam Binsz <adam@adambinsz.com>
2015-12-16 17:09:42 -0500

    Moved ParkType to its own file

--- Queue/Park.swift
+++ Queue/Park.swift
@@ -24,25 +24,3 @@ extension Park: Equatable {}
 func ==(lhs: Park, rhs: Park) -> Bool {
     return lhs.hashValue == rhs.hashValue
 }
-
-enum ParkType: Int, CustomStringConvertible {
-    case MagicKingdom = 80007944
-    case Epcot = 80007838
-    case HollywoodStudios = 80007998
-    case AnimalKingdom = 80007823
-    
-    static let allValues = [.MagicKingdom, .Epcot, .HollywoodStudios, .AnimalKingdom] as [ParkType]
-    
-    var description: String {
-        switch self {
-        case .MagicKingdom:
-            return "Magic Kingdom"
-        case .Epcot:
-            return "Epcot"
-        case .HollywoodStudios:
-            return "Hollywood Studios"
-        case .AnimalKingdom:
-            return "Animal Kingdom"
-        }
-    }
-}
